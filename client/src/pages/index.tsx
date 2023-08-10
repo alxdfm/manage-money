904e66b7105630f3f162fd0a8091036572eeb243
Alexandre Machado <alexandre_fmachado@hotmail.com>
2023-08-09 22:40:27 -0300

    created

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Managemoney</title>
+        <meta name="description" content="Managing my money" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <main className={`${inter.className}`}></main>
+    </>
+  );
+}
