678705808fd1d992857791807758c79bf76e9794
Alexandre Machado <alexandre_fmachado@hotmail.com>
2023-08-09 23:17:04 -0300

    create base ui

---
+++ client/src/styles/globals.ts
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  width: 1080px;
+  height: 200px;
+  margin-top: 8px;
+  border: 1px solid;
+`;
